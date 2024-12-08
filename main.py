30d882fe6cbb836688ab6b09fff8aa4d3ba07c86
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-12-08 02:19:16 +0100

    Add Telegram photo upload for XKCD comic

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 from dotenv import load_dotenv
 import os
+from telegram import Bot
+import asyncio
 
 load_dotenv()
-client_id = os.getenv("CLIENT_ID")
 
-print(f"Ваш client_id: {client_id}")
+telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
 comic_url = "https://xkcd.com/353/info.0.json"
 
@@ -19,8 +21,20 @@ comic_alt_text = comic_data['alt']
 image_response = requests.get(comic_image_url)
 image_response.raise_for_status()
 
-with open("xkcd_comic.png", "wb") as file:
+comic_filename = "xkcd_comic.png"
+with open(comic_filename, "wb") as file:
     file.write(image_response.content)
 
-print("Комментарий к комиксу:", comic_alt_text)
-print("Комикс скачан и сохранён как xkcd_comic.png")
+
+async def send_telegram_photo():
+    bot = Bot(token=telegram_bot_token)
+    with open(comic_filename, "rb") as comic_file:
+        await bot.send_photo(
+            chat_id=telegram_chat_id,
+            photo=comic_file,
+            caption=comic_alt_text
+        )
+    print("Комикс отправлен в Telegram!")
+
+
+asyncio.run(send_telegram_photo())
